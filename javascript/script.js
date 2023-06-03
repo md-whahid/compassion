6d3695d6e8d257ebe409abd323add7bcedf4ad28
md-whahid <mdislamwhahid@gmail.com>
2023-06-03 17:47:43 +0600

    commit 002

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,10 @@
 const navBar = document.getElementById("navba-btn");
 
 function hideMenu() {
-	navBar.style.right = "-200px";
+	navBar.style.right = "-1000px";
+	navBar.style.display = "none";
 }
 function showMenu() {
 	navBar.style.right = "0";
+	navBar.style.display = "block";
 }
